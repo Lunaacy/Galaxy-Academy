56f6b0d107772b58c4c32c964708a7252d03b1dc
00019210 <elbayevamira@gmail.com>
2025-07-11 01:47:18 +0500

    Fixed all images

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -8,16 +8,15 @@ const Header = () => {
     <header className={styles.custom}>
       <nav className={styles.navContent}>
         <ul className={styles.navLinks}>
-          <li>About us</li>
+          <li>
+            <Link href='/'>Home Page</Link>
+          </li>
           <li>
             <Link href='/profile'>Profile</Link>
           </li>
-          <li>Diary</li>
         </ul>
         <div className={styles.authButtons}>
-          <Link href='/login'>
-            <Button text="Log In" className={styles.loginBtn} />
-          </Link>
+          <Link href='/login' className='p-[0.5rem_2.5rem] border-2 rounded-[8px] border-[#A095E3] text-[#A095E3] font-medium'> Log In </Link>
           <Link href='/signup'>
             <Button text='Sign Up' className={styles.loginBtn}/>
           </Link>
--- src/components/profile/Profile.tsx
+++ src/components/profile/Profile.tsx
@@ -1,31 +1,28 @@
 import { Tablet } from "./tablet/Tablet";
 import { Container } from "../shared/container/container";
 import AstronautHead from "@/public/images/profile/user-view/head/boy-dark.svg";
-import AstronautSuit from "@/public/images/profile/user-view/suit/suit_test.svg";
-import moonImage from "@/public/images/profile/moon.webp";
+import AstronautSuit from "@/public/images/profile/user-view/suit/suit_blue.svg";
+import MoonImage from "@/public/images/profile/svg/planet_profile.png";
 import styles from "./Profile.module.scss";
 import Image from "next/image";
 import Header from '../header/header';
-import { Suspense } from "react";
 
 export const Profile = () => {
   return (
     <div className={styles.mainContainer}>
       <Header />
       <Container>
-        <Suspense fallback={<div>Loading...</div>}>
           <Tablet />
-        </Suspense>
       </Container>
       <Image
-        src={moonImage}
+        src={MoonImage}
         width={500}
         height={500}
         alt="moon"
         className={styles.moon}
       />
-      <AstronautHead className="w-[160px] h-auto absolute bottom-[488px] left-[140px] z-30" />
-      <AstronautSuit className="w-[160px] h-auto absolute bottom-[370px] left-[140px] z-20" />
+      <AstronautHead className="w-[260px] h-auto absolute bottom-[561px] left-[140px] z-30" />
+      <AstronautSuit className="w-[260px] h-auto absolute bottom-[370px] left-[140px] z-20" />
     </div>
   );
 };
