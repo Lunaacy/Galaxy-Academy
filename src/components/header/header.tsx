53ed1e3a40a76889b59b735655ae59be6068b254
Alfat1989 <Alfat1989@users.noreply.github.com>
2025-07-14 01:17:48 +0500

    feat: add logo

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,29 +1,45 @@
-import React from 'react'
-import styles from './Header.module.scss'
-import Link from 'next/link'
-import Button from '../button/page'
+import React from "react";
+import styles from "./Header.module.scss";
+import Link from "next/link";
+import Button from "../button/page";
+import Logo from "@/public/images/svg/logo-en.svg";
+import LogoCosmos from "@/public/images/svg/uzcosmos_logo_white.svg";
 
 const Header = () => {
   return (
     <header className={styles.custom}>
       <nav className={styles.navContent}>
+        <div className="flex items-center justify-between gap-10">
+          <Link href="/">
+            <Logo className="h-[30px] w-auto" />
+          </Link>
+          <Link href="https://uzspace.uz/ru" target="_blank">
+            <LogoCosmos className="h-[40px] w-auto" />
+          </Link>
+        </div>
         <ul className={styles.navLinks}>
           <li>
-            <Link href='/'>Home Page</Link>
+            <Link href="/">Home Page</Link>
           </li>
           <li>
-            <Link href='/profile'>Profile</Link>
+            <Link href="/profile">Profile</Link>
           </li>
         </ul>
         <div className={styles.authButtons}>
-          <Link href='/login' className='p-[0.5rem_2.5rem] border-2 rounded-[8px] border-[#A095E3] text-[#A095E3] font-medium'> Log In </Link>
-          <Link href='/signup'>
-            <Button text='Sign Up' className={styles.loginBtn}/>
+          <Link
+            href="/login"
+            className="p-[0.5rem_2.5rem] border-2 rounded-[8px] border-[#A095E3] text-[#A095E3] font-medium"
+          >
+            {" "}
+            Log In{" "}
+          </Link>
+          <Link href="/signup">
+            <Button text="Sign Up" className={styles.loginBtn} />
           </Link>
         </div>
       </nav>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
